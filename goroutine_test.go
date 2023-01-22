a626597427104df7f7e91da8a79eac00a0c6eb07
Hanif <hanif.hakim@spesolution.com>
2023-01-22 20:42:04 +0700

    Test how light goroutine is

--- goroutine_test.go
+++ goroutine_test.go
@@ -16,3 +16,15 @@ func TestCreateGoroutine(t *testing.T) {
 
 	time.Sleep(1 * time.Second) // will freeze 1 second
 }
+
+func DisplayNumber(number int) {
+	fmt.Println("Display", number)
+}
+
+func TestManyGoroutine(t *testing.T) {
+	for i := 0; i < 100000; i++ {
+		go DisplayNumber(i) // will print random because use goroutine
+	}
+
+	// time.Sleep(10 * time.Second)
+}
